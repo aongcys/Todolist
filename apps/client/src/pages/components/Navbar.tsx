c840b12d664df2136091790dad37f936d3395a90
aongcys <aong477478@gmail.com>
2025-09-19 22:51:20 +0700

    basic

---
+++ apps/client/src/pages/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-center h-10 items-center'>
+      hello kub
+    </div>
+  );
+}
+
+export default Navbar;
---
+++ apps/server/src/api/user/public/public.dto.ts
@@ -0,0 +1,12 @@
+export class Register {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
+export class login {
+  email: string;
+  password: string;
+}
